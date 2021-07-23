0f5497e731bcd6c3c58afb95a6cf0eeb422f222f
zhanbao2000 <zhanbao2000@gmail.com>
2021-07-23 11:31:37 +0800

    尝试使用新的日志分割方案

--- public_module/mb2pkg_mokalogger/__init__.py
+++ public_module/mb2pkg_mokalogger/__init__.py
@@ -4,7 +4,7 @@ __version__ = '2.0.0'
 
 import logging
 import os
-import time
+import re
 from logging.handlers import TimedRotatingFileHandler
 
 from .config import Config
@@ -14,8 +14,6 @@ LOGDIR = Config().logger_absdir
 
 class Log(object):
     def __init__(self, logger=None):
-        now = time.localtime(time.time())
-
         # 创建一个logger
         self.logger = logging.getLogger(logger)
         self.logger.setLevel(logging.DEBUG)
@@ -23,6 +21,8 @@ class Log(object):
         self.log_dir = LOGDIR
         self.log_path = os.path.join(self.log_dir, 'log')
         fh = TimedRotatingFileHandler(self.log_path, when='midnight', interval=1, backupCount=14, encoding='utf-8')  # 这个是python3的
+        fh.suffix = '%Y-%m-%d.log'
+        fh.extMatch = re.compile(r'^\d{4}-\d{2}-\d{2}.log$')
         fh.setLevel(logging.INFO)
 
         # 再创建一个handler，用于输出到控制台
