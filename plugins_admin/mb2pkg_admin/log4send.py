af82d71c8b046e9e8d7468417e5f1980be41cc34
zhanbao2000 <zhanbao2000@gmail.com>
2021-08-11 14:47:55 +0800

    在log_after_bot_send进行预处理，避免因mai-bot使用base64发送图片时，导致整个图片base64被记入日志最终导致日志文件庞大

--- plugins_admin/mb2pkg_admin/log4send.py
+++ plugins_admin/mb2pkg_admin/log4send.py
@@ -1,7 +1,7 @@
-from typing import Optional, Dict, Any
+from typing import Optional, Dict, Any, Union
 
 from nonebot.adapters import Bot
-from nonebot.adapters.cqhttp import MessageEvent, Event
+from nonebot.adapters.cqhttp import MessageEvent, Event, MessageSegment
 from nonebot.matcher import Matcher
 from nonebot.typing import T_State
 
@@ -12,7 +12,22 @@ log = getlog()
 
 async def log_after_bot_send(bot: Bot, exception: Optional[Exception], api: str, data: Dict[str, Any], result: Any):
     if api == 'send_msg':
-        log.info(f'Bot发送消息，meta={data}，result={result}')
+        # 因mai-bot会使用base64发送图片，导致整个图片base64被记入日志，因此在这里预处理
+        fin_data = {}
+        msg = []
+        for k, v in data.items():
+            if k == 'message':
+                if isinstance(v, list):
+                    for msgsmt in v:
+                        if msgsmt.type == 'image' and 'file' in msgsmt.data and msgsmt.data['file'].startswith('base64://'):
+                            msg.append('base64 image')
+                        else:
+                            msg.append(msgsmt)
+                else:
+                    msg.append(v)
+            else:
+                fin_data[k] = v
+        log.info(f'Bot发送消息，meta={fin_data}，msg={msg}，result={result}')
 
 
 async def log_before_exec_command(matcher: Matcher, bot: Bot, event: Event, state: T_State):
