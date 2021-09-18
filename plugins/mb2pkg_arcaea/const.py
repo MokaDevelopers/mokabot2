c20255dda130d21fa3165faa73c3e717ba90580d
lostDeers <syarocia@gmail.com>
2021-09-18 22:36:27 +0800

    优化了TC表解析，增加了PM表解析

--- plugins/mb2pkg_arcaea/const.py
+++ plugins/mb2pkg_arcaea/const.py
@@ -1,16 +1,17 @@
+from typing import Optional
+
+from bs4 import BeautifulSoup
 from nonebot import on_command
 from nonebot.adapters import Bot
 from nonebot.adapters.cqhttp import MessageSegment, MessageEvent
 from pydantic import BaseModel
-from bs4 import BeautifulSoup
-from bs4.element import Tag
-from typing import Optional
+
 from .config import Config
 
 match_twitter_const = on_command('const8', aliases={'const9', 'const10'}, priority=5)
 match_wiki_const = on_command('定数表', priority=5)  # TODO 使用arc中文维基的定数表
 match_wiki_TC = on_command('tc表', priority=5)  # TODO 使用arc中文维基的tc表(解析已完成，需要接入聊天)
-match_wiki_PM = on_command('pm表', priority=5)  # TODO 使用arc中文维基的pm表
+match_wiki_PM = on_command('pm表', priority=5)  # TODO 使用arc中文维基的pm表(解析已完成，需要接入聊天)
 
 
 class SongModel(BaseModel):
@@ -27,13 +28,23 @@ class TCDifficultyModel(BaseModel):
 
 
 class TCModel(BaseModel):
-    authors: list[str,str]
+    authors: list[str, str]
     difficulties_list: list[TCDifficultyModel]
 
 
 class ConstModel(BaseModel):
     const: str
     songs: list[SongModel]
+
+
+class PMDifficultyModel(BaseModel):
+    difficulty: str
+    songs: list[SongModel]
+
+
+class PMModel(BaseModel):
+    authors: str
+    difficulties_list: list[PMDifficultyModel]
 
 
 @match_twitter_const.handle()
@@ -51,17 +62,17 @@ async def wiki_tc_handle(bot: Bot, event: MessageEvent):
 
 # ⚠️注意 部分歌曲无图片(icon_url = None)，所有歌曲无定数(const = None)，加载时需注意
 def tc_text_parse(text: str) -> Optional[TCModel]:
-    bs = BeautifulSoup(text, features='lxml')
+    bs = BeautifulSoup(text.replace('\n', ''), features='lxml')
     base_node = bs.select_one('#mw-content-text > div > table > tbody')
     authors = [bs.select_one('#mw-content-text > div > table > tbody > tr:nth-child(2) > td > a:nth-child(1)').string,
                bs.select_one('#mw-content-text > div > table > tbody > tr:nth-child(2) > td > a:nth-child(2)').string]
-    tr_list = [s for s in list(base_node.children) if s != '\n'][3:]
+    tr_list = list(base_node.children)[3:]
     temp_songs_list = []
     temp_diff_list = []
     temp_diff_info = {}
     for tr in tr_list:
         temp = tr.find('td', bgcolor=True)
-        songs = [m for m in list(tr.children) if isinstance(m, Tag)]
+        songs = list(tr.children)
         if temp:
             if len(temp_diff_info) > 0:
                 temp_diff_list.append(TCDifficultyModel(**temp_diff_info, songs=temp_songs_list))
@@ -72,13 +83,26 @@ def tc_text_parse(text: str) -> Optional[TCModel]:
             temp_diff_info['limit'] = next(strs)
             songs.remove(temp)
         for song in songs[0].children:
-            if isinstance(song, Tag):
-                raw_url = song.find('img')
-                icon_url = ('https://wiki.arcaea.cn/' + raw_url['src']) if raw_url else None
-                name = song.find('td', style=True).a.string
-                difficulty = song.find('b').string
-                temp_songs_list.append(SongModel(icon_url=icon_url, name=name, difficulty=difficulty))
+            raw_url = song.find('img')
+            icon_url = ('https://wiki.arcaea.cn/' + raw_url['src']) if raw_url else None
+            name = song.find('td', style=True).a.string
+            difficulty = song.find('b').string
+            temp_songs_list.append(SongModel(icon_url=icon_url, name=name, difficulty=difficulty))
     return TCModel(authors=authors, difficulties_list=temp_diff_list)
 
 
-
+def pm_text_parse(text: str) -> Optional[PMModel]:
+    bs = BeautifulSoup(text.replace('\n', ''), features='lxml')
+    base_node = bs.find('tbody')
+    tr_list = list(base_node.children)
+    authors = tr_list[1].find('td').string
+    diff_list = []
+    for tr in tr_list[3:]:
+        td_list = list(tr.children)
+        diff = td_list[0].string
+        songs = []
+        for song in td_list[1].children:
+            songs.append(SongModel(name=song.find('a')['title'], icon_url=song.find('img')['src'],
+                                   difficulty=song.find('b').string))
+        diff_list.append(PMDifficultyModel(difficulty=diff, songs=songs))
+    return PMModel(authors=authors, difficulties_list=diff_list)
