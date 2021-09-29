0ce38aaeac8760a6c615a7176a75cb75c4725ba4
zhanbao2000 <zhanbao2000@gmail.com>
2021-09-30 02:15:04 +0800

    当自动更新Arcaea中文维基的某些表时，记录日志

--- plugins/mb2pkg_arcaea/timing_updater.py
+++ plugins/mb2pkg_arcaea/timing_updater.py
@@ -1,8 +1,13 @@
-from nonebot import require
-from .const import tc_text_parse, save_model, pm_text_parse, const_text_parse
+from datetime import datetime
+
 import aiohttp
 import nonebot
-from datetime import datetime
+from nonebot import require
+
+from public_module.mb2pkg_mokalogger import getlog
+from .const import tc_text_parse, save_model, pm_text_parse, const_text_parse
+
+log = getlog()
 
 scheduler = require('scheduler')
 data_absdir = nonebot.get_driver().config.data_absdir
@@ -11,16 +16,19 @@ data_absdir = nonebot.get_driver().config.data_absdir
 async def tc_update():
     async with aiohttp.request('GET', 'https://wiki.arcaea.cn/index.php/TC%E9%9A%BE%E5%BA%A6%E8%A1%A8') as resp:
         save_model(tc_text_parse(await resp.text()), 'tc.yaml', data_absdir)
+    log.info('自动更新Arcaea中文维基TC表成功')
 
 
 async def pm_update():
     async with aiohttp.request('GET', 'https://wiki.arcaea.cn/index.php/PM%E9%9A%BE%E5%BA%A6%E8%A1%A8') as resp:
         save_model(pm_text_parse(await resp.text()), 'pm.yaml', data_absdir)
+    log.info('自动更新Arcaea中文维基PM表成功')
 
 
 async def const_update():
     async with aiohttp.request('GET', 'https://wiki.arcaea.cn/index.php/%E5%AE%9A%E6%95%B0%E8%AF%A6%E8%A1%A8') as resp:
         save_model(const_text_parse(await resp.text()), 'const.yaml', data_absdir)
+    log.info('自动更新Arcaea中文维基定数表成功')
 
 
 scheduler.add_job(tc_update, hours=24, next_run_time=datetime.now())
