6a2851d3cebfe38ae6a81c739934e2437a0a488e
zhanbao2000 <zhanbao2000@gmail.com>
2022-01-08 19:43:15 +0800

    新增随机选择插件

---
+++ plugins/mb2pkg_choice/main.py
@@ -0,0 +1,18 @@
+from random import choice
+
+from nonebot import on_command
+from nonebot.adapters import Bot
+from nonebot.adapters.cqhttp import MessageEvent
+
+from public_module.mb2pkg_mokalogger import getlog
+
+match_random_choice = on_command('moka选择', priority=5)
+
+log = getlog()
+
+
+@match_random_choice.handle()
+async def random_choice_handle(bot: Bot, event: MessageEvent):
+    args = str(event.get_message()).strip()
+    items: list[str] = args.split()
+    await bot.send(event, f'moka的选择是：{choice(items)}')
