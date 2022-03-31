3efd3b9ea4f080875ccb5a474ceb2fb1f57e294d
zhanbao2000 <zhanbao2000@gmail.com>
2022-04-01 02:55:01 +0800

    修复vndb_update在日志中的输出

--- plugins/mb2pkg_vndb/auto_update.py
+++ plugins/mb2pkg_vndb/auto_update.py
@@ -21,10 +21,10 @@ async def vndb_update() -> None:
     update_cmd = f'bash {os.path.join(plugin_absdir, "auto_update.sh")} {res_absdir}'
     # proc = await asyncio.create_subprocess_shell(update_cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
     proc = await asyncio.create_subprocess_shell(update_cmd, stderr=asyncio.subprocess.PIPE)
-    stdout, stderr = await proc.communicate()
-    log.info(f'[{update_cmd!r} exited with {proc.returncode}]')
-    if stderr:
-        log.error(f'[stderr]\n{stderr.decode()}')
+    await proc.communicate()
+    log.info(f'{update_cmd!r} exited with {proc.returncode}')
+    # if stderr.decode():
+    #     log.error(f'[stderr]\n{stderr.decode()}')
 
     log.info('vndb资源已自动更新')
     log.info('定时任务已处理完成 耗时%.3fs' % (time.time() - start))
