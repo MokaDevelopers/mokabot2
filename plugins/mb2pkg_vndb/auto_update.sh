5a25dd045bcdf77430261db27b80160f106756e3
zhanbao2000 <zhanbao2000@gmail.com>
2022-04-02 14:37:35 +0800

    修复自动更新脚本的相对路径问题

--- plugins/mb2pkg_vndb/auto_update.sh
+++ plugins/mb2pkg_vndb/auto_update.sh
@@ -1,4 +1,4 @@
 wget -P /tmp https://dl.vndb.org/dump/vndb-db-latest.tar.zst
 zstd -d --rm /tmp/vndb-db-latest.tar.zst
-tar -xf /tmp/vndb-db-latest.tar db/chars db/chars_vns db/staff_alias TIMESTAMP db/vn db/vn_titles -C "$1"
+tar -xf /tmp/vndb-db-latest.tar plugins/mb2pkg_vndb/res/db/chars plugins/mb2pkg_vndb/res/db/chars_vns plugins/mb2pkg_vndb/res/db/staff_alias plugins/mb2pkg_vndb/res/TIMESTAMP plugins/mb2pkg_vndb/res/db/vn plugins/mb2pkg_vndb/res/db/vn_titles -C "$1"
 rm /tmp/vndb-db-latest.tar
