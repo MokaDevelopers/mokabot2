9f0c0efa8a03daa6a3defe27e84d6439e760f011
zhanbao2000 <zhanbao2000@gmail.com>
2022-04-08 15:50:43 +0800

    更新帮助页面url

--- plugins/mb2pkg_manual/manual.py
+++ plugins/mb2pkg_manual/manual.py
@@ -7,4 +7,4 @@ match_moka_manual = on_command('man', aliases={'manual', 'help'}, priority=5)
 
 @match_moka_manual.handle()
 async def moka_manual_handle(bot: Bot, event: MessageEvent):
-    await bot.send(event, '请参考该在线文档\nhttps://docs.arisa.moe/mokabot2')
+    await bot.send(event, '请参考该在线文档\nhttps://docs-mokabot.arisa.moe')
