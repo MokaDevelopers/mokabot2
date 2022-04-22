9f8f686907437b6ae04af53f1440eee236a1fae5
zhanbao2000 <zhanbao2000@gmail.com>
2022-04-23 01:13:08 +0800

    变更检测bot离线逻辑

--- plugins_admin/mb2pkg_admin/alive.py
+++ plugins_admin/mb2pkg_admin/alive.py
@@ -1,11 +1,17 @@
+import asyncio
+
 import aiohttp
+import nonebot
 from nonebot.adapters import Bot
 
 from .config import Config
 
 
 async def call_tgbot(bot: Bot) -> None:
-    await aiohttp.ClientSession().get(
-        f'https://api.telegram.org/bot{Config().token}/sendMessage',
-        params={'chat_id': Config().chat_id, 'text': f'寄！'}
-    )
+    # 60 秒后如果 bot 没有重连（体现为 bots 字典为空），则认为 bot 寄了
+    await asyncio.sleep(60)
+    if not nonebot.get_bots():
+        await aiohttp.ClientSession().get(
+            f'https://api.telegram.org/bot{Config().token}/sendMessage',
+            params={'chat_id': Config().chat_id, 'text': f'寄！'}
+        )
