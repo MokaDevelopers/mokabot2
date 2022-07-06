390764e0f69fa8447337d16834224c3a3c593909
zhanbao2000 <zhanbao2000@gmail.com>
2022-07-07 01:00:07 +0800

    将等待判断bot寄了的时间变更为3分钟

--- plugins_admin/mb2pkg_admin/alive.py
+++ plugins_admin/mb2pkg_admin/alive.py
@@ -9,7 +9,7 @@ from .config import Config
 
 async def call_tgbot(bot: Bot) -> None:
     # 60 秒后如果 bot 没有重连（体现为 bots 字典为空），则认为 bot 寄了
-    await asyncio.sleep(60)
+    await asyncio.sleep(60 * 3)
     if not nonebot.get_bots():
         await aiohttp.ClientSession().get(
             f'https://api.telegram.org/bot{Config().token}/sendMessage',
