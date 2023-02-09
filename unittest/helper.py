f9a952996350d63349ebc1de0aaa7d15fd1d37c1
zhanbao2000 <zhanbao2000@gmail.com>
2023-02-09 22:10:36 +0800

    为包装的单元测试添加is_reply参数

--- unittest/helper.py
+++ unittest/helper.py
@@ -5,7 +5,7 @@
 from time import time
 from typing import Union, Optional
 
-from nonebot.adapters.onebot.v11 import MessageEvent, Message, MessageSegment, Event
+from nonebot.adapters.onebot.v11 import MessageEvent, Message, MessageSegment
 
 AnyMessage = Union[Message, MessageSegment, str]
 
@@ -61,19 +61,28 @@ class TestMatcherSession:
     async def __aexit__(self, exc_type, exc_val, exc_tb):
         pass
 
-    async def test_event(self, event: Event):
-        raise NotImplementedError
-
-    async def test_reply(
+    async def test_send(
             self,
-            message: AnyMessage,
-            reply: Optional[AnyMessage] = None,
+            msg_send_to_bot: AnyMessage,
+            msg_send_by_bot: Optional[AnyMessage] = None,
             user_id: int = 1,
+            is_reply: bool = False,
     ):
+        """
+        给定发送的消息事件，bot应当对该事件发送一条期望的消息
+
+        :param msg_send_to_bot: 发送给 bot 的消息
+        :param msg_send_by_bot: bot 应当发送的消息
+        :param user_id: 发送消息的用户 id
+        :param is_reply: bot 是否为回复消息或直接发送消息
+        """
+
         async with self.app.test_matcher(self.matcher) as ctx:
             bot = ctx.create_bot()
-            event = FakePrivateMessageEvent(user_id, message)
+            event = FakePrivateMessageEvent(user_id, msg_send_to_bot)
             ctx.receive_event(bot, event)
-            if reply:
-                ctx.should_call_send(event, reply, result=True)
+            if is_reply and msg_send_by_bot is not None:
+                msg_send_by_bot = MessageSegment.reply(event.message_id) + msg_send_by_bot
+            if msg_send_by_bot:
+                ctx.should_call_send(event, msg_send_by_bot, result=True)
                 ctx.should_finished()
