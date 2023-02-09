dc52d7d439cd0cd0d47bdfadc53caea6231096b0
zhanbao2000 <zhanbao2000@gmail.com>
2023-02-09 19:32:59 +0800

    删除旧的 mb2pkg_admin 插件合集

--- plugins_admin/mb2pkg_admin/__init__.py
+++
@@ -1,9 +0,0 @@
-__author__ = '秋葉亜里沙 https://github.com/zhanbao2000'
-
-__version__ = '2.0.0'
-
-__all__ = ['broadcast', 'hooks', 'request', 'nonebot_plugin_manager']
-
-from . import *
-
-# TODO 需要更灵活的bot设置系统
