0a1872afa8514b25dc9dae89c4c634fa8185298d
zhanbao2000 <zhanbao2000@gmail.com>
2023-02-10 00:56:07 +0800

    变更bot启动文件

--- bot.py
+++ bot.py
@@ -2,21 +2,12 @@ import os
 import sys
 
 import nonebot
-from nonebot.adapters.cqhttp import Bot as CQHTTPBot
-from nonebot.log import logger
+from nonebot.adapters.onebot.v11 import Adapter
+from nonebot.log import logger, logger_id
 
 
 def init():
     nonebot.init()
-
-
-def add_global_config():
-    config = nonebot.get_driver().config
-    config.bot_absdir = os.path.abspath('.')  # r'D:\Python\mokabot2'
-    config.data_absdir = os.path.abspath('data')  # r'D:\Python\mokabot2\data'
-    config.userdata_absdir = os.path.abspath('data/users')  # r'D:\Python\mokabot2\data\users'
-    config.groupdata_absdir = os.path.abspath('data/groups')  # r'D:\Python\mokabot2\data\groups'
-    config.temp_absdir = os.path.abspath('temp')  # r'D:\Python\mokabot2\temp'
 
 
 def logger_filter(record: dict) -> bool:
@@ -28,7 +19,7 @@ def logger_filter(record: dict) -> bool:
 
 
 def change_logger():
-    logger.remove()
+    logger.remove(logger_id)
     default_format = (
         '<g>{time:MM-DD HH:mm:ss}</g> '
         '[<lvl>{level}</lvl>] '
@@ -36,10 +27,11 @@ def change_logger():
         '<c>{function}:{line}</c> | '
         '{message}'
     )
+    log_level = nonebot.get_driver().config.log_level
 
-    # 控制台 logger，保留 INFO 级别以上的日志
+    # 控制台 logger，保留 log_level 级别以上的日志
     logger.add(
-        sys.stdout, level='INFO', format=default_format, filter=logger_filter, colorize=True, diagnose=True,
+        sys.stdout, level=log_level, format=default_format, filter=logger_filter, colorize=True, diagnose=True,
     )
     # 文件 logger，仅保留 WARNING 级别以上的日志
     logger.add(
@@ -50,18 +42,17 @@ def change_logger():
 
 def add_adapter():
     driver = nonebot.get_driver()
-    driver.register_adapter('cqhttp', CQHTTPBot)
+    driver.register_adapter(Adapter)
 
 
 def load_plugins():
-    nonebot.load_plugins('plugins_admin')
-    nonebot.load_plugins('plugins')
-    # nonebot.load_plugin('mb2pkg_api')
+    nonebot.load_plugins('src/utils')
+    nonebot.load_plugins('src/plugins_preload')
+    nonebot.load_plugins('src/plugins')
 
 
 if __name__ == '__main__':
     init()
-    add_global_config()
     change_logger()
     add_adapter()
     load_plugins()
