8aa39ce0a1b6ef9057f8b1b7342cd3ee38017bcc
zhanbao2000 <zhanbao2000@gmail.com>
2023-05-10 22:34:44 +0800

    紧急修复

--- src/plugins/mokabot_bandori/user.py
+++ src/plugins/mokabot_bandori/user.py
@@ -1,3 +1,4 @@
+from loguru import logger
 from nonebot.adapters.onebot.v11 import MessageSegment
 
 from .bestdori.model import Language
@@ -8,4 +9,9 @@ from .utils import get_user_profile
 async def generate_user_profile_image(user_id: int, region: Language) -> MessageSegment:
     if not (profile := await get_user_profile(user_id, region)):
         return MessageSegment.text(f'该用户<{user_id}>不存在')
-    return MessageSegment.image(await SimpleUserProfileStyle(profile, user_id, region).generate())
+    try:
+        img = await SimpleUserProfileStyle(profile, user_id, region).generate()
+    except Exception as e:
+        logger.exception(e)
+        return MessageSegment.text(f'生成图片时出现异常：{e}')
+    return MessageSegment.image(img)
