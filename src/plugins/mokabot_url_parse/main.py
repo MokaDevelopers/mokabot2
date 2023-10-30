0e20979728118e0bfd0fa192f9ab7ba4b5fd9d90
zhanbao2000 <zhanbao2000@gmail.com>
2023-10-30 17:35:26 +0800

    修改 event.raw_message 为 str(event.message)

--- src/plugins/mokabot_url_parse/main.py
+++ src/plugins/mokabot_url_parse/main.py
@@ -18,7 +18,8 @@ class SetParse:
         self._last_parse_dict: dict[int, tuple[str, str]] = {}  # subtype, suburl, 标记最后一次解析内容以防止多个机器人循环解析
 
     async def handle(self, event: MessageEvent):
-        subtype, suburl = await self._parse.preprocesse(event.raw_message)
+        # TODO 其他地方的 event.raw_message 也要对应修改
+        subtype, suburl = await self._parse.preprocesse(str(event.message))
         message_from = event.group_id if isinstance(event, GroupMessageEvent) else event.user_id
 
         if self._last_parse_dict.get(message_from, None) == (subtype, suburl):
